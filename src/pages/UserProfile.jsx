10fb6d735fec3980d6de255a7ac78b2318b9c628
stzkdigitalmedia <tanuthakue2@gmail.com>
2026-01-26 19:52:02 +0530

    init

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -320,7 +320,7 @@ const UserProfile = () => {
                   <p className="font-medium text-[16px]">{user?.branchName || t('notAssigned')}</p>
                 </div>
               </div>
-              <div className="flex items-center gap-3 p-1 px-2 bg-[#1b1b1b] rounded-lg">
+              <div className="hidden sm:flex items-center gap-3 p-1 px-2 bg-[#1b1b1b] rounded-lg">
                 <CreditCard className="w-5 h-5" />
                 <div className='hidden sm:block'>
                   <p className="text-[12px] leading-none text-gray-300">{t('balance')}</p>
