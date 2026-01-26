4a5804df49ccb4106b94e38da4568b1e0b4fef33
stzkdigitalmedia <tanuthakue2@gmail.com>
2026-01-26 19:49:21 +0530

    init

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -244,8 +244,8 @@ const UserProfile = () => {
           </div>
           <div className='p-2 mt-2'>
             <div>
-              <p className="sm:block hidden text-[12px] text-gray-300">{t('balance')}</p>
-              <p className="sm:block hidden font-medium text-[18px] leading-none text-green-600">₹{user?.balance?.toLocaleString() || '0'}</p>
+              {/* <p className="hidden text-[12px] text-gray-300">{t('balance')}</p>
+              <p className="hidden font-medium text-[18px] leading-none text-green-600">₹{user?.balance?.toLocaleString() || '0'}</p> */}
               <div className='grid grid-cols-2 gap-4 mt-4'>
                 <button
                   onClick={() => {
@@ -322,7 +322,7 @@ const UserProfile = () => {
               </div>
               <div className="flex items-center gap-3 p-1 px-2 bg-[#1b1b1b] rounded-lg">
                 <CreditCard className="w-5 h-5" />
-                <div>
+                <div className='hidden sm:block'>
                   <p className="text-[12px] leading-none text-gray-300">{t('balance')}</p>
                   <p className="font-medium text-[16px] text-green-600">₹{user?.balance?.toLocaleString() || '0'}</p>
                 </div>
